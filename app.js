a936233436b1605002b3ab8b947f8fcd20b5dc33
ricardinho <ricardolopes18@yahoo.com.br>
2022-09-16 23:21:31 +0200

    make it work with different dates

--- app.js
+++ app.js
@@ -18,72 +18,108 @@ const NAMED_COLORS = [
   CHART_COLORS.grey,
 ];
 
+// defining all graphs and contexts then I can destroy them later to be reused
+var ctxGastos = document.getElementById('gastosChart').getContext('2d');
+var chartGastos = new Chart(ctxGastos, {})
+var ctxFixos = document.getElementById('fixosChart').getContext('2d');
+var chartFixos = new Chart(ctxFixos, {})
+var ctxVariaveis = document.getElementById('variaveisChart').getContext('2d');
+var chartVariaveis = new Chart(ctxVariaveis, {})
+
+
+
+console.log(mes, ano)
+
+function destroyCharts() {
+  chartGastos.destroy()
+  chartFixos.destroy()
+  chartVariaveis.destroy()
+}
+
+
 // the app.js file containing all the app code
-fetch('http://localhost:8000/gastos')
-.then((response) => gastosData = response.json())
-.then(data => gastosData = data[0])
-.then(() => {
+function generateGraphs() {
+  let mes = document.getElementById("mes").value
+  let ano = document.getElementById("ano").value
 
-  const dataGastosChart = {
-    labels: ["Total"],
-    datasets: [{
-      axis: 'y',
-      label: "Variaveis",
-      data: [gastosData['Variaveis']],
-      fill: true,
-      backgroundColor: [
-        CHART_COLORS.red
-      ],
-      borderWidth: 0
-    },
-    {
-      axis: 'y',
-      label: "Fixos",
-      data: [gastosData['Fixos']],
-      fill: true,
-      backgroundColor: [
-        CHART_COLORS.orange
-      ],
-      borderWidth: 0
-    }]
-  };
+  let urlFixos = `http://localhost:8000/fixos/${mes}/${ano}/`
+  let urlGastos = `http://localhost:8000/gastos/${mes}/${ano}/`
+  let urlVariaveis = `http://localhost:8000/variaveis/${mes}/${ano}/`
 
-  const configGastos = {
-    type: 'bar',
-    data: dataGastosChart,
-    options: {
-      indexAxis: 'y',
-      scales: {
+  destroyCharts()
+
+  fetch(urlGastos)
+      .then((response) => gastosData = response.json())
+      .then(data => {
+        delete data['mes']
+        delete data['ano']
+        gastosData = data
+      })
+      .then(() => {
+
+
+        const dataGastosChart = {
+          labels: ["Total"],
+          datasets: [{
+            axis: 'y',
+            label: "Variaveis",
+            data: [gastosData['Variaveis']],
+            fill: true,
+            backgroundColor: [
+              CHART_COLORS.red
+            ],
+            borderWidth: 0
+          },
+            {
+              axis: 'y',
+              label: "Fixos",
+              data: [gastosData['Fixos']],
+              fill: true,
+              backgroundColor: [
+                CHART_COLORS.orange
+              ],
+              borderWidth: 0
+            }]
+        };
+
+        const configGastos = {
+          type: 'bar',
+          data: dataGastosChart,
+          options: {
+            indexAxis: 'y',
+            scales: {
               y: {
-                  stacked: true,
-                  beginAtZero: false,
-                  GeolocationCoordinates: {display: false},
+                stacked: true,
+                beginAtZero: false,
+                GeolocationCoordinates: {display: false},
               },
               x: {
-                  max: gastosData['Salario'],
-                  stacked: true,
-                  grid: {display: false},
-                }
-          },
-          plugins: {
-            legend: {
-              position: 'top',
+                max: gastosData['Salario'],
+                stacked: true,
+                grid: {display: false},
+              }
             },
-            title: {
-              display: true,
-              text: 'Diferença de Gastos'
+            plugins: {
+              legend: {
+                position: 'top',
+              },
+              title: {
+                display: true,
+                text: 'Diferença de Gastos'
+              }
             }
           }
-    }
-  };
+        };
 
-  const ctxGastos = document.getElementById('gastosChart').getContext('2d');
-  const chartGastos = new Chart(ctxGastos, configGastos)
 
-})
-.catch((error) => {
-  console.error('There has been a problem with your fetch operation:', error);
-});
+
+
+        chartGastos = new Chart(ctxGastos, configGastos)
+
+      })
+      .catch((error) => {
+        console.error('There has been a problem with your fetch operation:', error);
+      });
 
 // const gastosData = {
 //   'Fixos': 2517.87,
@@ -91,53 +127,55 @@ fetch('http://localhost:8000/gastos')
 //   'Salario': 4942,
 // }
 
-fetch('http://localhost:8000/fixos')
-.then((response) => fixosData = response.json())
-.then(data => fixosData = data[0])
-.then(() => {
-  const dataFixos = {
-    labels: Object.keys(fixosData),
-    datasets: [
-      {
-        label: 'Dataset 1',
-        data: Object.values(fixosData),
-      }
-    ]
-  };
+  fetch(urlFixos)
+      .then((response) => fixosData = response.json())
+      .then(data => {
+        delete data['mes']
+        delete data['ano']
+        fixosData = data
+      })
+      .then(() => {
+        const dataFixos = {
+          labels: Object.keys(fixosData),
+          datasets: [
+            {
+              label: 'Dataset 1',
+              data: Object.values(fixosData),
+            }
+          ]
+        };
 
-  const configFixos = {
-    type: 'doughnut',
-    data: dataFixos,
-    options: {
-      backgroundColor: [
-        CHART_COLORS.orange,  // Bar 1
-        CHART_COLORS.red,     // Bar 2
-        CHART_COLORS.purple,  // Bar 3
-        CHART_COLORS.blue,    // Bar 4
-        CHART_COLORS.yellow,  // bar 5
-        CHART_COLORS.grey,    // bar 6
-      ],
-      responsive: true,
-      plugins: {
-        legend: {
-          position: 'top',
-        },
-        title: {
-          display: true,
-          text: 'Custos Fixos do Mês'
-        }
-      }
-    },
-  };
+        const configFixos = {
+          type: 'doughnut',
+          data: dataFixos,
+          options: {
+            backgroundColor: [
+              CHART_COLORS.orange,  // Bar 1
+              CHART_COLORS.red,     // Bar 2
+              CHART_COLORS.purple,  // Bar 3
+              CHART_COLORS.blue,    // Bar 4
+              CHART_COLORS.yellow,  // bar 5
+              CHART_COLORS.grey,    // bar 6
+            ],
+            responsive: true,
+            plugins: {
+              legend: {
+                position: 'top',
+              },
+              title: {
+                display: true,
+                text: 'Custos Fixos do Mês'
+              }
+            }
+          },
+        };
 
-  
-  const ctxFixos = document.getElementById('fixosChart').getContext('2d');
-  const chartFixos = new Chart(ctxFixos, configFixos)
+        chartFixos = new Chart(ctxFixos, configFixos)
 
-})
-.catch((error) => {
-  console.error('There has been a problem with your fetch operation:', error);
-});
+      })
+      .catch((error) => {
+        console.error('There has been a problem with your fetch operation:', error);
+      });
 
 // const fixosData = {
 //   'Contas': 400,
@@ -148,50 +186,56 @@ fetch('http://localhost:8000/fixos')
 //   'Outros': 350,
 // }
 
-fetch('http://localhost:8000/variaveis')
-.then((response) => variaveisData = response.json())
-.then(data => variaveisData = data[0])
-.then(() => {
-  const dataVariaveis = {
-    labels: Object.keys(variaveisData),
-    datasets: [
-      {
-        label: 'Dataset 1',
-        data: Object.values(variaveisData),
-      }
-    ]
-  };
+  fetch(urlVariaveis)
+      .then((response) => variaveisData = response.json())
+      .then(data => {
+        delete data['mes']
+        delete data['ano']
+        variaveisData = data
+      })
+      .then(() => {
+        const dataVariaveis = {
+          labels: Object.keys(variaveisData),
+          datasets: [
+            {
+              label: 'Dataset 1',
+              data: Object.values(variaveisData),
+            }
+          ]
+        };
 
-  const configVariaveis = {
-    type: 'doughnut',
-    data: dataVariaveis,
-    options: {
-      backgroundColor: [
-        'rgba(255, 99, 132, 0.5)',  // Bar 1
-        'rgba(54, 162, 235, 0.5)',  // Bar 2
-        'rgba(255, 206, 86, 0.5)',  // Bar 3
-        'rgba(75, 192, 192, 0.5)',  // Bar 4
-      ],
-      responsive: true,
-      plugins: {
-        legend: {
-          position: 'top',
-        },
-        title: {
-          display: true,
-          text: 'Custos Variáveis do Mês'
-        }
-      }
-    },
-  };
+        const configVariaveis = {
+          type: 'doughnut',
+          data: dataVariaveis,
+          options: {
+            backgroundColor: [
+              'rgba(255, 99, 132, 0.5)',  // Bar 1
+              'rgba(54, 162, 235, 0.5)',  // Bar 2
+              'rgba(255, 206, 86, 0.5)',  // Bar 3
+              'rgba(75, 192, 192, 0.5)',  // Bar 4
+            ],
+            responsive: true,
+            plugins: {
+              legend: {
+                position: 'top',
+              },
+              title: {
+                display: true,
+                text: 'Custos Variáveis do Mês'
+              }
+            }
+          },
+        };
 
+        chartVariaveis = new Chart(ctxVariaveis, configVariaveis)
 
-  const ctxVariaveis = document.getElementById('variaveisChart').getContext('2d');
-  const chartVariaveis = new Chart(ctxVariaveis, configVariaveis)
-})
-.catch((error) => {
-  console.error('There has been a problem with your fetch operation:', error);
-});
+      })
+      .catch((error) => {
+        console.error('There has been a problem with your fetch operation:', error);
+      });
+}
+//
+// generateGraphs();
 
 // const variaveisData = {
 //   'Compras': 1133.89,
